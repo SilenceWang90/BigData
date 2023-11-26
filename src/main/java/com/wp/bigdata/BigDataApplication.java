80f838d3f63639ff64b2d5e1072635d26d9a13a7
Wangpeng@90 <wangpeng0717@sina.cn>
2023-11-26 10:09:10 +0800

    大数据

--- src/main/java/com/wp/bigdata/BigDataApplication.java
+++ src/main/java/com/wp/bigdata/BigDataApplication.java
@@ -1,9 +1,16 @@
 package com.wp.bigdata;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Description
  * @Author admin
  * @Date 2023/11/26 9:47
  */
+@SpringBootApplication
 public class BigDataApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BigDataApplication.class, args);
+    }
 }
